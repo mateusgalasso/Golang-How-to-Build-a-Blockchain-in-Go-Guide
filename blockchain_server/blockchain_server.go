5b14f2d02bbbf7c0034b07ababe0b816df9770c9
Mateus Galasso <mateusgalasso@fbb.org.br>
2022-12-20 16:07:21 -0300

    Creating API that will let the blockchain server return the cryptocurrency total

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -140,10 +140,27 @@ func (bcs *BlockchainServer) StartMine(w http.ResponseWriter, req *http.Request)
 	return
 }
 
+func (bcs *BlockchainServer) Amount(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		blockchainAddress := req.URL.Query().Get("blockchain_address")
+		amount := bcs.GetBlockchain().CalculateTotalAmount(blockchainAddress)
+		ar := &block.AmountResponse{amount}
+		m, _ := ar.MarshalJSON()
+		w.Header().Add("Content-Type", "application/json")
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Printf("ERROR: Http method is invalid")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	return
+}
+
 func (bcs *BlockchainServer) Run() {
 	http.HandleFunc("/", bcs.GetChain)
 	http.HandleFunc("/transactions", bcs.Transactions)
 	http.HandleFunc("/mine", bcs.Mine)
 	http.HandleFunc("/mine/start", bcs.StartMine)
+	http.HandleFunc("/amount", bcs.Amount)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
 }
